563aabe23efb3388bfbdc8f43a8fde696a9c4fb4
Matt Manning <matt.manning@gmail.com>
2014-09-14 15:59:28 -0400

    First commit.

---
+++ app.rb
@@ -0,0 +1,34 @@
+require 'mail'
+require 'sinatra'
+
+options = { :address              => "smtp.gmail.com",
+            :port                 => 587,
+            :domain               => 'gmail.com',
+            :user_name            => 'captesting157',
+            :password             => 'cap157soar',
+            :authentication       => 'plain',
+            :enable_starttls_auto => true  }
+
+Mail.defaults do
+  delivery_method :smtp, options
+end
+
+
+get '/' do
+  'Hello world!'
+end
+
+post '/test_request' do
+  message =  "Name: #{params[:name]}\n"
+  message += "CAPID: #{params[:capid]}\n"
+  message += "Email: #{params[:email]}\n"
+  message += "Message: #{params[:message]}\n"
+
+
+  mail = Mail.deliver do
+    from    'captesting157@gmail.com'
+    to      'ngaio.warrington@yahoo.com'
+    subject 'This is a test email'
+    body    message
+  end
+end
